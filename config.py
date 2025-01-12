655e0c7045448e70957bf4ed7d253783c0929505
folieds <yosnkooke@gmail.com>
2025-01-13 00:38:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DB_NAME = os.environ.get("DB_NAME", "TA_Links_Bot")
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002149354979"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002146121321"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002146121321"))
 
 
 SECONDS = int(os.getenv("SECONDS", "1200")) # auto delete in seconds
